c54184b0a65079b8ca33441ccc5ee7356de061bc
hasan sohib <aresuma307@gmail.com>
2023-02-15 16:33:31 +0700

    week2

--- Week2/app/src/main/java/com/sohibLmj/layoutweek2/MainActivity.java
+++ Week2/app/src/main/java/com/sohibLmj/layoutweek2/MainActivity.java
@@ -3,12 +3,29 @@ package com.sohibLmj.layoutweek2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editUsername,editPasswoard;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editUsername = findViewById(R.id.usernametxt);
+        editPasswoard = findViewById(R.id.usernametxt);
+        btnLogin = findViewById(R.id.loginbtn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dataUsername = editUsername.getText().toString();
+                Toast.makeText(MainActivity.this, "Datanya : "+dataUsername,Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
